ae15cd2058a8b2ebef95007b42cc1ac6cd2f1cf1
James <applejobs9@gmail.com>
2011-09-22 19:32:14 +0800

    Revert 1ef337ea52d410cc89a37ef95f98ae5db5d8f743^..HEAD

--- src/com/larc/waveform/WaveformActivity.java
+++ src/com/larc/waveform/WaveformActivity.java
@@ -14,17 +14,13 @@ import com.larc.waveform.data.ReceivedData;
 public class WaveformActivity extends Activity {
 	private static final int DEFAULT_SIZE = 50;
 	private static final int WAVEFORM_COUNT = 4;
-
+	
 	/** Called when the activity is first created. */
 
 	private WaveformView[] mWaveformArray;
-
-	private LinearLayout mWaveformContainer;
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> parent of 1ef337e... Revert 97bf37c5f70f6b1bd02e5b71737f7c8e76d40492^..HEAD
+	private LinearLayout mWaveformContainer;
+	
 	private Handler mHandler;
 	private Button mButtonPause;
 	private Button mButtonEEG;
@@ -32,54 +28,38 @@ public class WaveformActivity extends Activity {
 
 	private ReceivedData[] mDbsData = new ReceivedData[WAVEFORM_COUNT];
 	private ReceivedData[] mEegData = new ReceivedData[WAVEFORM_COUNT];
-
-	private int mEegShown = 0;
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_waveform);
 		mDbsData[0] = new ReceivedData();
 		mEegData[0] = new ReceivedData();
-
+		
 		mButtonPause = (Button) findViewById(R.id.buttonPause);
 		mButtonEEG = (Button) findViewById(R.id.buttonEEG);
 		mButtonDBS = (Button) findViewById(R.id.buttonDBS);
-
+		
 		mWaveformArray = new WaveformView[WAVEFORM_COUNT];
 		mWaveformContainer = (LinearLayout) findViewById(R.id.waveformContainer);
-
-		LinearLayout.LayoutParams params = new LayoutParams(
-				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1f);
-
-		for (int i = 0; i < WAVEFORM_COUNT; i++) {
+		
+		LinearLayout.LayoutParams params = 
+				new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1f);
+		for(int i=0; i< WAVEFORM_COUNT ; i++){
 			mWaveformArray[i] = new WaveformView(this);
 			mWaveformContainer.addView(mWaveformArray[i], params);
 		}
+		
 
-		for (WaveformView wave : mWaveformArray) {
-
-			mWaveformArray[1].setLineColor(0, Color.GREEN);
-
-			mWaveformArray[2].setLineColor(0, Color.BLUE);
-
-			mWaveformArray[3].setLineColor(0, R.color.weak_yellow);
-
+		for(WaveformView wave : mWaveformArray){
 			wave.start();
 		}
-
+		
 		mButtonPause.setOnClickListener(new Button.OnClickListener() {
 
 			public void onClick(View view) {
-				for (WaveformView wave : mWaveformArray) {
-					int on = 1;
-					if (on == 1) {
-						wave.stop();
-						on = 0;
-					} else {
-						wave.start();
-						on = 1;
-					}
+				for(WaveformView wave : mWaveformArray){
+					wave.stop();
 				}
 			}
 		});
@@ -87,34 +67,25 @@ public class WaveformActivity extends Activity {
 		mButtonEEG.setOnClickListener(new Button.OnClickListener() {
 
 			public void onClick(View view) {
-				if (mEegShown == 0) {
-					showEEG();
-				}
+				showEEG();
 			}
 		});
-
+		
 		mButtonDBS.setOnClickListener(new Button.OnClickListener() {
-
+			
 			public void onClick(View v) {
-				if (mEegShown == 1) {
-					showDBS();
-				}
+				showDBS();
 			}
 		});
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 1ef337e... Revert 97bf37c5f70f6b1bd02e5b71737f7c8e76d40492^..HEAD
 		mHandler = new Handler();
 		mHandler.post(mPushDataRunnable);
 	}
-
-	public void showDBS() {
-		mEegShown = 0;
-		for (WaveformView wave : mWaveformArray) {
+	
+	private void showDBS(){
+		for(WaveformView wave : mWaveformArray){
 			wave.removeAllDataSet();
 			wave.createNewDataSet(DEFAULT_SIZE);
-			int[] dataArray = mDbsData[0].getData(DEFAULT_SIZE);
+			int [] dataArray = mDbsData[0].getData(DEFAULT_SIZE);
 			wave.setData(0, dataArray);
 		}
 		mWaveformArray[1].setLineColor(0, Color.GREEN);
@@ -123,13 +94,12 @@ public class WaveformActivity extends Activity {
 
 		mWaveformArray[3].setLineColor(0, R.color.weak_yellow);
 	}
-
-	public void showEEG() {
-		mEegShown = 1;
-		for (WaveformView wave : mWaveformArray) {
+	
+	public void showEEG(){
+		for(WaveformView wave : mWaveformArray){
 			wave.removeAllDataSet();
 			wave.createNewDataSet(DEFAULT_SIZE);
-			int[] dataArray = mEegData[0].getData(DEFAULT_SIZE);
+			int [] dataArray = mEegData[0].getData(DEFAULT_SIZE);
 			wave.setData(0, dataArray);
 		}
 		mWaveformArray[1].setLineColor(0, Color.GREEN);
@@ -144,7 +114,7 @@ public class WaveformActivity extends Activity {
 			int data = (int) (System.currentTimeMillis() % 100) - 50;
 			mDbsData[0].add(data);
 			mEegData[0].add(-data);
-			for (WaveformView wave : mWaveformArray) {
+			for(WaveformView wave : mWaveformArray){
 				wave.setCurrentData(0, data);
 			}
 			mHandler.postDelayed(this, 10);
--- src/com/larc/waveform/data/ReceivedData.java
+++ src/com/larc/waveform/data/ReceivedData.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 public class ReceivedData {
 	private ArrayList<Integer> mData = new ArrayList<Integer>();
-
-	public ArrayList<Integer> getDataArray() {
+	
+	public ArrayList<Integer> getDataArray(){
 		return mData;
 	}
-
-	public void add(int value) {
+	
+	public void add(int value){
 		mData.add(value);
 	}
-
-	public int[] getData(int size) {
+	
+	public int[] getData(int size){
 		int[] data = new int[size];
 		int offset = mData.size() - size;
 		int bound = Math.min(size, mData.size());
-		for (int i = 0; i < bound; i++) {
-			if (i + offset < 0) {
+		for(int i=0 ; i < bound; i++){
+			if(i+offset<0){
 				data[i] = 0;
-			} else {
-				data[i] = mData.get(i + offset);
+			}else{
+				data[i] = mData.get(i+offset);
 			}
 		}
 		return data;
