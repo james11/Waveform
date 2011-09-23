3d4063385f07d0d0092d751c82428e609b1055ee
James <applejobs9@gmail.com>
2011-09-24 03:05:25 +0800

    Fix button bugs, add channel names
    received data still null

--- src/com/larc/waveform/WaveformActivity.java
+++ src/com/larc/waveform/WaveformActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,20 +14,22 @@ import android.widget.TextView;
 import com.larc.waveform.data.ReceivedData;
 
 public class WaveformActivity extends Activity {
-	private static final int DEFAULT_SIZE = 50;
+	private static final int DEFAULT_SIZE = 100;
 	private static final int WAVEFORM_COUNT = 4;
-	
+
 	private static final int COLOR_TEXT_NORMAL = Color.BLACK;
 	private static final int COLOR_TEXT_SELECTED = 0xFFFF9900;
-	
+
 	private static final int SIGNAL_EEG = 0;
 	private static final int SIGNAL_DBS = 1;
-	
+
 	/** Called when the activity is first created. */
 
 	private WaveformView[] mWaveformArray;
+	private TextView[] mWaveformName;
 
 	private LinearLayout mWaveformContainer;
+	private LinearLayout mWaveformBlock;
 
 	private Handler mHandler;
 	private Button mButtonPause;
@@ -38,6 +41,7 @@ public class WaveformActivity extends Activity {
 	private ReceivedData[] mEegData = new ReceivedData[WAVEFORM_COUNT];
 
 	private int mSignal = SIGNAL_EEG;
+	private int mSignalCheck = SIGNAL_DBS;
 	private boolean mPause = false;
 
 	@Override
@@ -55,14 +59,31 @@ public class WaveformActivity extends Activity {
 		mTextView = (TextView) findViewById(R.id.myTextView);
 
 		mWaveformArray = new WaveformView[WAVEFORM_COUNT];
+		mWaveformName = new TextView[WAVEFORM_COUNT];
+
 		mWaveformContainer = (LinearLayout) findViewById(R.id.waveformContainer);
+		mWaveformBlock = (LinearLayout) findViewById(R.id.waveformBlock);
 
 		LinearLayout.LayoutParams params = new LayoutParams(
 				LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1f);
 
 		for (int i = 0; i < WAVEFORM_COUNT; i++) {
+
+			mWaveformName[i] = new TextView(this);
+			mWaveformName[i].setText("CH"+(i+1));
+			mWaveformName[i].setTextSize(40);
+			mWaveformName[i].setTextColor(0xffff9900);
+			mWaveformName[i].setGravity(android.view.Gravity.CENTER);
+
 			mWaveformArray[i] = new WaveformView(this);
+			
+			 int HorizontalScrollView = 1;
+			 mWaveformContainer.setOrientation(HorizontalScrollView);
+			 mWaveformBlock.setOrientation(HorizontalScrollView);
+
 			mWaveformContainer.addView(mWaveformArray[i], params);
+			mWaveformBlock.addView(mWaveformName[i], params);
+
 		}
 
 		for (WaveformView wave : mWaveformArray) {
@@ -74,12 +95,17 @@ public class WaveformActivity extends Activity {
 			mWaveformArray[3].setLineColor(0, R.color.weak_yellow);
 
 			mTextView.setText("4-Channel DBS Signals");
+
+			mButtonDBS.setTextColor(COLOR_TEXT_SELECTED);
+			mButtonEEG.setTextColor(COLOR_TEXT_NORMAL);
+
 			wave.start();
 		}
 
 		mButtonPause.setOnClickListener(new Button.OnClickListener() {
 
 			public void onClick(View view) {
+				Log.v("Wave", "ButtonPauseClicked");
 				pauseAndStart();
 			}
 		});
@@ -90,29 +116,35 @@ public class WaveformActivity extends Activity {
 
 		mHandler = new Handler();
 		mHandler.post(mPushDataRunnable);
-		
+
 		refreshSignal();
 	}
-	
-	private Button.OnClickListener mButtonClickListener = new Button.OnClickListener(){
-		@Override
+
+	private Button.OnClickListener mButtonClickListener = new Button.OnClickListener() {
+		// @Override
 		public void onClick(View v) {
 			int id = v.getId();
-			switch(id){
+
+			switch (id) {
 			case R.id.buttonDBS:
 				mSignal = SIGNAL_DBS;
 				break;
 			case R.id.buttonEEG:
 			default:
 				mSignal = SIGNAL_EEG;
+
 				break;
 			}
-			refreshSignal();
+			if (mSignal != mSignalCheck) {
+				refreshSignal();
+			}
+
 		}
 	};
-	
-	private void refreshSignal(){
-		switch(mSignal){
+
+	private void refreshSignal() {
+		mSignalCheck = mSignal;
+		switch (mSignal) {
 		case SIGNAL_DBS:
 			showDBS();
 			mButtonDBS.setTextColor(COLOR_TEXT_SELECTED);
@@ -126,7 +158,7 @@ public class WaveformActivity extends Activity {
 			mTextView.setText("4-Channel EEG Signals");
 			break;
 		}
-		
+
 	}
 
 	public void pauseAndStart() {
