c6d472a8adfff709aa2933d87efb4463a92102e9
james <applejobs9@gmail.com>
2012-05-04 21:58:51 +0800

    Smart Phone Version1.1

--- src/com/larc/waveform/AppVersionInfoActivity.java
+++ src/com/larc/waveform/AppVersionInfoActivity.java
@@ -21,6 +21,16 @@ public class AppVersionInfoActivity extends Activity {
 	private static final int COLOR_TEXT_Editor_DEPARTMENT = 0xFF777777;
 	private static final int COLOR_TEXT_HIGHLIGHT = 0xFFFF9900;
 
+	private static final int INFO_TEXT_SIZE = 20; // Tablet:35, SmartPhone: 20
+	private static final int VERSION_TEXT_SIZE = 20; // Tablet:35, SmartPhone:
+														// 20
+	private static final int DATE_TEXT_SIZE = 15;// Tablet:20, SmartPhone: 15
+	private static final int INTRODUCTION_TEXT_SIZE = 15;// Tablet:25,
+															// SmartPhone: 15
+	private static final int EDITOR_TEXT_SIZE = 20;// Tablet:30, SmartPhone: 20
+	private static final int DEPARTMENT_TEXT_SIZE = 20;// Tablet:30, SmartPhone:
+														// 20
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,17 +44,17 @@ public class AppVersionInfoActivity extends Activity {
 		mAppInfoTextview
 				.setText("WaveformActivity Application Version Information\n");
 		mAppInfoTextview.setTextColor(COLOR_TEXT_TITLE);
-		mAppInfoTextview.setTextSize(35);
+		mAppInfoTextview.setTextSize(INFO_TEXT_SIZE);
 
 		mVersionTextview = (TextView) findViewById(R.id.myVersionTextView);
 		mVersionTextview.setText("Version : 1.1");
 		mVersionTextview.setTextColor(COLOR_TEXT_HIGHLIGHT);
-		mVersionTextview.setTextSize(35);
+		mVersionTextview.setTextSize(VERSION_TEXT_SIZE);
 
 		mDateTextview = (TextView) findViewById(R.id.myDateTextView);
 		mDateTextview.setText("\t\t\t(Updated at 2012/04/29)\n");
 		mDateTextview.setTextColor(COLOR_TEXT_NORMAL);
-		mDateTextview.setTextSize(20);
+		mDateTextview.setTextSize(DATE_TEXT_SIZE);
 
 		mIntroductionTextview = (TextView) findViewById(R.id.myIntroductionTextView);
 		mIntroductionTextview
@@ -55,18 +65,18 @@ public class AppVersionInfoActivity extends Activity {
 						+ "\t4. Heart rate count Fixed\n"
 						+ "\t5. Buttons redesigned\n");
 		mIntroductionTextview.setTextColor(COLOR_TEXT_NORMAL);
-		mIntroductionTextview.setTextSize(25);
+		mIntroductionTextview.setTextSize(INTRODUCTION_TEXT_SIZE);
 
 		mEditorTextview = (TextView) findViewById(R.id.myEditorTextView);
 		mEditorTextview.setText("Editor : \n" + "\t\tChun-Chieh Chan \n");
 		mEditorTextview.setTextColor(COLOR_TEXT_Editor_DEPARTMENT);
-		mEditorTextview.setTextSize(30);
+		mEditorTextview.setTextSize(EDITOR_TEXT_SIZE);
 
 		mDepartmentTextview = (TextView) findViewById(R.id.myDepartmentTextView);
 		mDepartmentTextview.setText("Department : \n"
 				+ "\t\tNTHU EE LaRC Hp_group");
 		mDepartmentTextview.setTextColor(COLOR_TEXT_Editor_DEPARTMENT);
-		mDepartmentTextview.setTextSize(30);
+		mDepartmentTextview.setTextSize(DEPARTMENT_TEXT_SIZE);
 
 		// Set result CANCELED in case the user backs out
 		setResult(Activity.RESULT_CANCELED);
